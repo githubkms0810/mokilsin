c00d90dde4f9ac5bb0e8d0f592472e3f6e8b1b70
kimmincastle <koreakms0810@gmail.com>
2018-03-27 14:27:59 +0900

    페이지 추가

---
+++ application/modules/mokilsin/controllers/Base.php
@@ -0,0 +1,48 @@
+<?php 
+namespace mokilsin;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Base extends \Base_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        // $this->get = true;
+        // $this->list = true;
+        // $this->add = true;
+        // $this->update = true;
+        // $this->delete = true;
+        // $this->noDisplay = true;
+    }
+    
+    public function birth()
+    {
+        $data["content_view"] = "base/birth";
+        $this->template->render($data);
+    }
+
+//     public function get($id)
+//     {
+//         parent::get($id);
+//     }
+//     public function list()
+//     {
+//         parent::list();
+//     }
+//     public function add()
+//     {
+//         parent::add();
+//     }
+//     public function update($id)
+//     {
+//         parent::update($id);
+//     }
+//     public function delete($id)
+//     {
+//         parent::_ajaxDelete($id);
+//     }
+}
+
+/* End of file Admin.php */
+
+?>
