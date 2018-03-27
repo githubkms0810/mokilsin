edbacf8b4713d37ca514bfec69199df2755d2a9e
임수빈 <sbin910@gmail.com>
2018-03-27 11:24:28 +0900

    menu

--- application/views/base/template.php
+++ application/views/base/template.php
@@ -20,37 +20,67 @@
         <div class="wrap">
             <!-- header -->
             <header class="header fixed">
-                <div>
-                    <h1><a href="index.html"><img src="/public/mokilsin/images/logo.png" alt="동요 작가 목일신"></a></h1>
-                    <ul class="gnb">
-                        <li><a href="birth.php">생애/업적</a></li>
-                        <li class="gnb__item">
-                            <a href="list.php">작품집</a>
+			<div>
+				<h1><a href="index.html"><img src="../images/logo.png" alt="잡댄스"></a></h1>
+				<div class="gnb">
+                <ul>
+					<li>
+                        <a href="#">목일신</a>
                             <ul>
                                 <li>
-                                    <a style="font-size:12px; font-weight:400;" href="/freelancer/add">프리랜서 지원</a>
-                                    <a style="font-size:12px; font-weight:400;" href="/contact/add">업무 제휴 문의</a>
-                                    <a style="font-size:12px; font-weight:400;" href="/small/security">기밀 유지 정책</a>
+                                    <a style="font-size:12px; font-weight:400;" href="birth.php">생애/업적</a>
+                                    <a style="font-size:12px; font-weight:400;" href="/contact/add">작품집</a>
+                                    <a style="font-size:12px; font-weight:400;" href="/small/security">홍보 영상</a>
                                 </li>
                             </ul>
-                        </li>
-                        <li><a href="movie.php">동영상</a></li>
-                        <li><a href="introduce_music.php">목일신 동요제</a></li>
-                        <li><a href="introduce_poem.php">목일신 동시대회</a></li>
-                        <li class="m-menu login"><a href="#">등록하기</a></li>
-                        <li class="m-menu"><a href="#">마이페이지</a></li>
-                        <li class="m-menu"><a href="#">로그아웃</a></li>
-                    </ul>
-                    <div class="util">
-                        <a href="#" class="login">등록하기</a>
-                        <a href="#" class="join">마이페이지</a>
-                        <a href="#" class="join">로그아웃</a>
-                    </div>
-                    <a href="javascript:mMenuOn();" class="m-menu-btn">모바일 메뉴 열기</a>
-                    <a href="javascript:mMenuOff();" class="m-menu-btn_close">모바일 메뉴 닫기</a>
-                    <a href="javascript:mMenuOff();" class="m-menu-btn_close_x">모바일 메뉴 닫기</a>
+                    </li>
+					<li>
+                        <a href="list.php">목일신 동요제</a>
+                            <ul>
+                                <li>
+                                    <a style="font-size:12px; font-weight:400;" href="/freelancer/add">동요제 소개</a>
+                                    <a style="font-size:12px; font-weight:400;" href="/contact/add">참가 신청</a>
+                                    <a style="font-size:12px; font-weight:400;" href="/small/security">역대 수상자</a>
+                                </li>
+                            </ul>
+                    </li>
+                    <li>
+                        <a href="list.php">목일신 동시대회</a>
+                            <ul>
+                                <li>
+                                    <a style="font-size:12px; font-weight:400;" href="/freelancer/add">동시대회 소개</a>
+                                    <a style="font-size:12px; font-weight:400;" href="/contact/add">참가 신청</a>
+                                    <a style="font-size:12px; font-weight:400;" href="/small/security">역대 수상자</a>
+                                </li>
+                            </ul>
+                    </li>
+                    <li>
+                        <a href="list.php">커뮤니티</a>
+                            <ul>
+                                <li>
+                                    <a style="font-size:12px; font-weight:400;" href="/freelancer/add">공지사항</a>
+                                    <a style="font-size:12px; font-weight:400;" href="/contact/add">자료실</a>
+                                    <a style="font-size:12px; font-weight:400;" href="/small/security">자유 게시판</a>
+                                </li>
+                            </ul>
+                    </li>
+					<li class="m-menu login"><a href="#">등록하기</a></li>
+					<li class="m-menu"><a href="#">마이페이지</a></li>
+					<li class="m-menu"><a href="#">로그아웃</a></li>
+				</ul>
                 </div>
-            </header>
+				
+				<div class="util">
+					<a href="#" class="login">등록하기</a>
+					<a href="#" class="join">마이페이지</a>
+					<a href="#" class="join">로그아웃</a>
+				</div>
+				
+				<a href="javascript:mMenuOn();" class="m-menu-btn">모바일 메뉴 열기</a>
+				<a href="javascript:mMenuOff();" class="m-menu-btn_close">모바일 메뉴 닫기</a>
+				<a href="javascript:mMenuOff();" class="m-menu-btn_close_x">모바일 메뉴 닫기</a>
+			</div>
+		</header>
             <!-- /header -->
             <?=$this->load->views($content_view)?>
             <!-- footer -->
