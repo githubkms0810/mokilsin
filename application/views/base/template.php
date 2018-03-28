0121b1448dd0071a73c5fc717f52cf3b002d48e0
kimmincastle <koreakms0810@gmail.com>
2018-03-28 18:14:51 +0900

    다듬기

--- application/views/base/template.php
+++ application/views/base/template.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="ko">
     <head>
-        <title>동요 작가 목일신</title>
+        <title>동요작가 목일신</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=Edge">
         <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width">
@@ -64,18 +64,8 @@
                                 </li>
                             </ul>
                     </li>
-					<li class="m-menu login"><a href="#">등록하기</a></li>
-					<li class="m-menu"><a href="#">마이페이지</a></li>
-					<li class="m-menu"><a href="#">로그아웃</a></li>
 				</ul>
-                </div>
-				
-				<div class="util">
-					<a href="#" class="login">등록하기</a>
-					<a href="#" class="join">마이페이지</a>
-					<a href="#" class="join">로그아웃</a>
-				</div>
-				
+                </div>				
 				<a href="javascript:mMenuOn();" class="m-menu-btn">모바일 메뉴 열기</a>
 				<a href="javascript:mMenuOff();" class="m-menu-btn_close">모바일 메뉴 닫기</a>
 				<a href="javascript:mMenuOff();" class="m-menu-btn_close_x">모바일 메뉴 닫기</a>
@@ -87,7 +77,7 @@
             <div class="footer">
                 <div class="copy">
                     <p>우 59542 전남 고흥군 고흥읍 고흥군청로 1 / 대표전화 061-830-5898 / 팩스 061-830-5577</p>
-                    <p>&copy; 2015 Goheung Country. ALL RIGHTS RESERVED.</p>
+                    <p>&copy; 2018 Goheung Country. ALL RIGHTS RESERVED.</p>
                 </div>
             </div>
             <!-- /footer -->
