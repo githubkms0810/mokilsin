a89c232568f96aa7b410358dc5b3efe05bfc22cd
santutu <santutu@naver.com>
2018-03-30 22:33:19 +0900

    ㅇ

--- application/modules/applicationn/views/admin/search.php
+++ application/modules/applicationn/views/admin/search.php
@@ -25,7 +25,7 @@
         <?php endif; ?>
 			<br>
 		</form>
-        <a href="/admin/applicationn/excel?kind=동요&personalOrGroup=독창">동요/개인 다운로드</a>
+        <a href="/admin/applicationn/excel?kind=동요&personalOrGroup=독창">동요/독창 다운로드</a>
 <br>
 <a href="/admin/applicationn/excel?kind=동요&personalOrGroup=중창">동요/중창 다운로드</a>
 <br>
