e2a6e64291abc6fc87ab1bcf3e57acc4c03968c8
santutu <santutu@naver.com>
2018-04-02 16:44:46 +0900

    null 체크 2

--- application/modules/applicationn/views/admin/listOfPersonalAgitationExcel.php
+++ application/modules/applicationn/views/admin/listOfPersonalAgitationExcel.php
@@ -11,12 +11,12 @@
   <?php $i=0; foreach ( $rows as $key=>$row ): ?>
     <tr>
         <td style="text-align:center; font-size:20px; padding:5 20;  background-color:#d9e1f2" class="tg-agg0"><?=$i+1?></td>
-        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->성명)[0]?></td>
-        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->지역?></td>
-        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->학교)[0]?></td>
-        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->학년)[0]?></td>
-        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->자유곡?></td>
-        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->지정곡?></td>
+        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->성명)[0] ?? ""?></td>
+        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->지역 ?? ""?></td>
+        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->학교)[0] ?? ""?></td>
+        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->학년)[0] ?? ""?></td>
+        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->자유곡 ?? ""?></td>
+        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->지정곡 ?? ""?></td>
     </tr>
   <?php $i++;endforeach; ?>
 </table>
