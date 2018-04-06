a3c4bcccf41a9741c675c4bab2c5fdf530ea5d4b
kimmincastle <koreakms0810@gmail.com>
2018-04-06 10:39:03 +0900

    dd

--- application/views/base/template.php
+++ application/views/base/template.php
@@ -5,6 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=Edge">
         <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width">
+        <link rel="shortcut icon" href="/public/mokilsin/images/favicon.ico">
         <link rel="stylesheet" type="text/css" href="/public/mokilsin/css/jquery.bxslider.css" media="all">
         <link rel="stylesheet" type="text/css" href="/public/mokilsin/css/swiper.min.css" media="all">
         <link rel="stylesheet" type="text/css" href="/public/mokilsin/css/front-base.css" media="all">
