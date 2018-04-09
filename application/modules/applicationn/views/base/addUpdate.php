2066664b65a2fac4ef61e8e06a7226fde7e39d02
kimmincastle <koreakms0810@gmail.com>
2018-04-09 17:02:21 +0900

    dd

--- application/modules/applicationn/views/base/addUpdate.php
+++ application/modules/applicationn/views/base/addUpdate.php
@@ -150,22 +150,22 @@
 
                     <select name="지역" class="budget" style="width:33%; display:inline-block;">
                         <option selected="selected">서울특별시</option>
-                        <option selected="selected">인천광역시</option>
-                        <option selected="selected">대전광역시</option>
-                        <option selected="selected">광주광역시</option>
-                        <option selected="selected">대구광역시</option>
-                        <option selected="selected">울산광역시</option>
-                        <option selected="selected">부산광역시</option>
-                        <option selected="selected">세종특별자치시</option>
-                        <option selected="selected">경기도</option>
-                        <option selected="selected">충청남도</option>
-                        <option selected="selected">충청북도</option>
-                        <option selected="selected">전라남도</option>
-                        <option selected="selected">전라북도</option>
-                        <option selected="selected">경상남도</option>
-                        <option selected="selected">경상북도</option>
-                        <option selected="selected">강원도</option>
-                        <option selected="selected">제주특별자치도</option>
+                        <option>인천광역시</option>
+                        <option>대전광역시</option>
+                        <option>광주광역시</option>
+                        <option>대구광역시</option>
+                        <option>울산광역시</option>
+                        <option>부산광역시</option>
+                        <option>세종특별자치시</option>
+                        <option>경기도</option>
+                        <option>충청남도</option>
+                        <option>충청북도</option>
+                        <option>전라남도</option>
+                        <option>전라북도</option>
+                        <option>경상남도</option>
+                        <option>경상북도</option>
+                        <option>강원도</option>
+                        <option>제주특별자치도</option>
                     </select>
                 </div>
                 <div>
