52f3e8ce39c146257d12291cd14d49adb612c3be
임수빈 <sbin910@gmail.com>
2018-04-12 18:24:30 +0900

    동시엑셀

--- application/modules/applicationn/views/admin/listOfPersonalPeomExcel.php
+++ application/modules/applicationn/views/admin/listOfPersonalPeomExcel.php
@@ -5,6 +5,10 @@
   <th class="tg-agg0">지역</th>
   <th class="tg-agg0">학교</th>
   <th class="tg-agg0">학년</th>
+    <th class="tg-agg0">반</th>
+    <th class="tg-agg0">성별</th>
+      <th class="tg-agg0">지도자 성명</th>
+      <th class="tg-agg0">지도자 연락처</th>
 </tr>
 <?php $i=0; foreach ( $rows as $key=>$row ): ?>
   <tr>
@@ -13,6 +17,10 @@
       <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->지역 ?? ""?></td>
       <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->학교)[0] ?? ""?></td>
       <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->학년)[0] ?? ""?></td>
+      <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->반)[0] ?? ""?></td>
+      <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->성별)[0] ?? ""?></td>
+        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->지도교사및보호자성명 ?? ""?></td>
+        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->지도교사및보호자연락처 ?? ""?></td>
   </tr>
 <?php $i++;endforeach; ?>
 </table>
