7174a41f0ae604a00761a5a4d3ac5c227ad11325
임수빈 <sbin910@gmail.com>
2018-04-12 16:45:53 +0900

    ㅎ

--- application/modules/applicationn/views/admin/listOfPersonalAgitationExcel.php
+++ application/modules/applicationn/views/admin/listOfPersonalAgitationExcel.php
@@ -20,14 +20,11 @@
         <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->지역 ?? ""?></td>
         <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->학교)[0] ?? ""?></td>
         <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->학년)[0] ?? ""?></td>
-
-        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->개인성별 ?? ""?></td>
+        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=splitWithComma($row->성별)[0] ?? ""?></td>
         <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->지도교사및보호자성명 ?? ""?></td>
         <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->가창지도자연락처 ?? ""?></td>
         <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->가창지도자이메일 ?? ""?></td>
-        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->가창지도자주소 ?? ""?></td>
-
-        
+        <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->가창지도자주소 ?? ""?></td>        
         <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->자유곡 ?? ""?></td>
         <td style="text-align:center; font-size:20px; padding:5 20; " class="tg-yw4l"><?=$row->지정곡 ?? ""?></td>
     </tr>
